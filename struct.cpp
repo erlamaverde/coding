80b34182134ca41654335058f03c20751831a829
erlamaverde <erlamaverde@gmail.com>
2026-03-05 14:41:59 +0300

    Add files via upload

--- struct.cpp
+++ struct.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <vector>
 
 
 //STRUTTURE
@@ -21,43 +22,50 @@ std::ostream& operator<<(std::ostream& os, TStudente& s);
 //MAIN
 int main()
 {
-
-    TStudente persone[100];
+    TStudente PersonModel;
+    std::vector<TStudente> person;
 
     std::cout << "This is an agenda of people, before inserting tell me how many people would you like to insert [max should be 100]:\n";
     int NumP;
     std::cin >> NumP;
+    std::cin.ignore();
     std::cout << "Now insert the data, ";
 
     for(int i=0; i<NumP; i++){
         std::cout << "person " << i+1 << ":\n"; 
-        std::cin >> persone[i];       
+        std::cin >> PersonModel;
+        person.push_back(PersonModel);
     }
+    
+    //WHILE
+    bool running = true;
 
+    while(running){
+        
     std::cout << "now, watchu gon' do next?"
               << "\n[1] insert people"
               << "\n[2] print agenda\n";
     
     int option;
     std::cin >> option;
-    
-    while(true){
+
     switch(option){
         
         case 1:
             for(int i=0; i<NumP; i++){
                 std::cout << "\nperson " << i+1 << ":\n"; 
-                std::cin >> persone[i];       
+                std::cin >> PersonModel;
+                person.push_back(PersonModel);
             }
         break;
 
         case 2:
-            for(int i=0; i<NumP; i++){
+            for(int i=0; i<person.size(); i++){
                 std::cout << "\nperson " << i+1 << ":"; 
-                std::cout << persone[i]; 
+                std::cout << person[i];
             }
         break;
-        
+
         default:
             std::cout << "inserimento non valido";
         break;
