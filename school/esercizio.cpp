ba1d03ed3be43b78f53e42c003422a2b4c399bf2
ErLama <macc.matt.81>
2026-03-20 12:20:24 +0100

    scambio lettere

--- school/esercizio.cpp
+++ school/esercizio.cpp
@@ -1,5 +1,28 @@
-1 scambia lettere di una stringa
+//1 scambia lettere di una stringa
+#include <iostream>
+#include <string>
 
-2 traduci una parola in un vettore di ? per consonanti e * per vocali con il rispettivo numero
+int main(){
 
-3 traduci dal vettore a parola
+    
+
+
+    std::string p = "busatto";
+    int m = p.size() / 2;
+    if(p.size() % 2 != 0){
+        m++;
+    }
+
+    for(int i=0; i<m; i++){
+        std::swap(p[i], p[p.size()-1-i]);
+    }
+    std::cout << p;
+    std::cout << "\n" << p.size()/2;
+}
+
+//1.1 scambia le lettere reciproce
+
+
+//2 traduci una parola in un vettore di ? per consonanti e * per vocali con il rispettivo numero
+
+//3 traduci dal vettore a parola
