a126be9b009d682c708c929a61be93f971913746
ErLama Verde <erlamaverde@gmail.com>
2026-04-05 00:40:20 +0200

    creata cartella personal, cartella claude e file rubrica.cpp

---
+++ personal/claude/rubrica/rubrica.cpp
@@ -0,0 +1,15 @@
+//Rubrica telefonica: crea una struct Contatto con nome e numero, salva più contatti in un vettore e permetti di cercare un contatto per nome
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+struct Tcontatto{
+    std::string nome;
+    std::string cognome;
+    int numero;
+};
+
+int main(){
+    
+}
